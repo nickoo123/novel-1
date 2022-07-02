c45d5e2ebede96348127154439dc378451f5d4c5
yangyun <1410432625@qq.com>
2022-07-02 10:53:12 +0800

    update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 set -eu
 
-docker stop novels && docker rm novels
 docker system prune -a
 docker build -t novel .
+docker stop novels && docker rm novels
 docker run -itd --name novels -p 8081:8081 --restart=always -v /data/novel/up:/go/static/up novel:latest
